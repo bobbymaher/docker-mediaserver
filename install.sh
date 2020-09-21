1caeedf6bd6d523b4460cb652d41b793e950b052
Bobby Maher <bobby.maher@icabbi.com>
2020-09-21 21:03:40 +0100

    volumes

--- install.sh
+++ install.sh
@@ -9,8 +9,12 @@
     sudo apt-get install -y vim git
     git clone https://github.com/bobbymaher/docker-mediaserver.git
 
-sudo apt-get install hfsplus hfsutils hfsprogs gdisk -y
-sudo fsck hfsplus /dev/sda1
 
 sudo mkdir /mnt/external1
 sudo mount /dev/sda1 /mnt/external1
+
+sudo mkdir -p /mnt/external1/downloads/convert
+sudo mkdir -p /mnt/external1/downloads/DownloadCompleted
+sudo mkdir -p /mnt/external1/downloads/DownloadInProgress
+
+sudo chmod -R 777 /mnt/external1/downloads
