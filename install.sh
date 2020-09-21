a8ad9700e88c5d26c8b85a6cab5aa293b0aab176
Bobby Maher <bobby.maher@icabbi.com>
2020-09-21 21:49:02 +0100

    revert

--- install.sh
+++ install.sh
@@ -1,17 +1,12 @@
-  echo "for OSX NFS DRIVES ONLY"
-  
-    curl -sSL https://get.docker.com | sh
-    sudo usermod -aG docker pi
-    sudo apt-get install -y libffi-dev libssl-dev
-    sudo apt-get install -y python3 python3-pip
-    sudo apt-get remove python-configparser -y
-    sudo pip3 -v install docker-compose
-    sudo apt-get install -y vim git
-    git clone https://github.com/bobbymaher/docker-mediaserver.git
-    cd docker-mediaserver && mkdir handbrake && cd handbrake
-    git clone https://github.com/jlesage/docker-handbrake.git .
-    
 
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker pi
+sudo apt-get install -y libffi-dev libssl-dev
+sudo apt-get install -y python3 python3-pip
+sudo apt-get remove python-configparser -y
+sudo pip3 -v install docker-compose
+sudo apt-get install -y vim git
+git clone https://github.com/bobbymaher/docker-mediaserver.git
 
 sudo mkdir /mnt/external1
 sudo mount /dev/sda1 /mnt/external1
